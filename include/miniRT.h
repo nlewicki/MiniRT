d8f617124d743b0ce8aa5271c719df3498c90f80
Nico <nlewicki@student.42heilbronn.de>
2025-05-13 12:16:56 +0200

    norminette include

--- include/miniRT.h
+++ include/miniRT.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2025/05/13 11:50:41 by nlewicki         ###   ########.fr       */
+/*   Created: 2025/05/13 12:14:07 by nlewicki          #+#    #+#             */
+/*   Updated: 2025/05/13 12:14:10 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include "../lib/MLX42/include/MLX42/MLX42.h"
 # include "../lib/libft/get_next_line.h"
 # include "../lib/libft/libft.h"
-# include "structs.h"
 # include "fcntl.h"
+# include "structs.h"
 # include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -38,60 +38,60 @@
 # define REFLECTION_MAX 1
 # define MAX_DEPTH 4
 
-int					parse_rt_file(char *filename, t_scene *scene);
-void				init_scene(t_scene *scene);
-void				exit_error(char *msg);
-void				free_scene(t_scene *scene);
+int		parse_rt_file(char *filename, t_scene *scene);
+void	init_scene(t_scene *scene);
+void	exit_error(char *msg);
+void	free_scene(t_scene *scene);
 
-void				ft_free_split(char **split);
-double				parse_double(char *str, double min, double max, int *error);
-t_color				parse_color(char *str, int *error);
-t_vec3				parse_position(char *str, int *error);
-t_vec3				parse_orientation(char *str, int *error);
+void	ft_free_split(char **split);
+double	parse_double(char *str, double min, double max, int *error);
+t_color	parse_color(char *str, int *error);
+t_vec3	parse_position(char *str, int *error);
+t_vec3	parse_orientation(char *str, int *error);
 
-void				parse_ambient(char **tokens, t_scene *scene);
-void				parse_camera(char **tokens, t_scene *scene);
-void				parse_cylinder(char **tokens, t_scene *scene);
-void				parse_light(char **tokens, t_scene *scene);
-void				parse_plane(char **tokens, t_scene *scene);
-void				parse_sphere(char **tokens, t_scene *scene);
-void				parse_cone(char **tokens, t_scene *scene);
+void	parse_ambient(char **tokens, t_scene *scene);
+void	parse_camera(char **tokens, t_scene *scene);
+void	parse_cylinder(char **tokens, t_scene *scene);
+void	parse_light(char **tokens, t_scene *scene);
+void	parse_plane(char **tokens, t_scene *scene);
+void	parse_sphere(char **tokens, t_scene *scene);
+void	parse_cone(char **tokens, t_scene *scene);
 
-void				convert_objects(t_scene *scene);
-double				hit_sphere(t_object *obj, const t_ray ray, t_hit *hit_info);
-t_color				checkerboard_sphere(t_sphere *sph, t_vec3 point);
-t_color				checkerboard_plane(t_plane *plane, t_vec3 point);
-double				hit_plane(t_object *obj, const t_ray ray, t_hit *hit_info);
-double				hit_cylinder(t_object *obj, const t_ray ray, t_hit *hit_info);
+void	convert_objects(t_scene *scene);
+double	hit_sphere(t_object *obj, const t_ray ray, t_hit *hit_info);
+t_color	checkerboard_sphere(t_sphere *sph, t_vec3 point);
+t_color	checkerboard_plane(t_plane *plane, t_vec3 point);
+double	hit_plane(t_object *obj, const t_ray ray, t_hit *hit_info);
+double	hit_cylinder(t_object *obj, const t_ray ray, t_hit *hit_info);
 double	hit_cone(t_object *obj, const t_ray ray, t_hit *hit_info);
-t_color				compute_lighting(t_miniRT *mini, t_hit hit);
-t_color trace_ray(t_miniRT *mini, t_ray ray, int depth);
+t_color	compute_lighting(t_miniRT *mini, t_hit hit);
+t_color	trace_ray(t_miniRT *mini, t_ray ray, int depth);
 
 double	solve_quadratic(double a, double b, double c);
 
 // utils
-t_vec3				vec_sub(t_vec3 a, t_vec3 b);
-t_vec3				vec_add(t_vec3 a, t_vec3 b);
-double				vec_dot(t_vec3 a, t_vec3 b);
-double				vec_skal(t_vec3 a, t_vec3 b);
-t_vec3				vec_normalize(t_vec3 v);
-t_vec3				vec_mul(t_vec3 v, double scalar);
-t_vec3				vec_cross(t_vec3 a, t_vec3 b);
-double				vec_length(t_vec3 v);
-t_vec3 vec_neg(t_vec3 v);
-t_vec3 vec_reflect(t_vec3 v, t_vec3 n);
-t_color color_mix(t_color a, t_color b, double factor);
-t_color color_scale(t_color c, double factor);
-t_color color_clamp(t_color c);
+t_vec3	vec_sub(t_vec3 a, t_vec3 b);
+t_vec3	vec_add(t_vec3 a, t_vec3 b);
+double	vec_dot(t_vec3 a, t_vec3 b);
+double	vec_skal(t_vec3 a, t_vec3 b);
+t_vec3	vec_normalize(t_vec3 v);
+t_vec3	vec_mul(t_vec3 v, double scalar);
+t_vec3	vec_cross(t_vec3 a, t_vec3 b);
+double	vec_length(t_vec3 v);
+t_vec3	vec_neg(t_vec3 v);
+t_vec3	vec_reflect(t_vec3 v, t_vec3 n);
+t_color	color_mix(t_color a, t_color b, double factor);
+t_color	color_scale(t_color c, double factor);
+t_color	color_clamp(t_color c);
 
-t_color checkerboard_cylinder(t_cylinder *cyl, t_vec3 point);
-t_color checkerboard_cone(t_cone *cone, t_vec3 point);
+t_color	checkerboard_cylinder(t_cylinder *cyl, t_vec3 point);
+t_color	checkerboard_cone(t_cone *cone, t_vec3 point);
 
 void	key_hook(mlx_key_data_t key, void *param);
 void	exit_mini(t_miniRT *mini);
 
 // render
-void				render_scene(mlx_image_t *img, t_miniRT *mini);
+void	render_scene(mlx_image_t *img, t_miniRT *mini);
 
 bool	handle_material_property(t_miniRT *mini, int key);
 bool	handle_checkerboard_toggle(t_miniRT *mini, int key);
@@ -99,7 +99,6 @@ void	handle_camera_rotation(t_miniRT *mini, int key, t_vec3 forward);
 void	handle_vertical_rotation(t_miniRT *mini, int key, t_vec3 forward);
 void	handle_horizontal_rotation(t_miniRT *mini, int key, t_vec3 forward);
 void	handle_camera_movement(t_miniRT *mini, int key, t_vec3 forward);
-
 
 t_color	checkerboard_sphere(t_sphere *sph, t_vec3 point);
 t_color	checkerboard_plane(t_plane *plane, t_vec3 point);
--- include/structs.h
+++ include/structs.h
@@ -1,4 +1,4 @@
-	/* ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   structs.h                                          :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 10:53:02 by nlewicki          #+#    #+#             */
-/*   Updated: 2025/05/08 11:57:57 by nlewicki         ###   ########.fr       */
+/*   Updated: 2025/05/13 12:16:23 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,26 @@
 
 # include "../lib/MLX42/include/MLX42/MLX42.h"
 
-// For RGB colors
 typedef struct s_color
 {
-	int				r;
-	int				g;
-	int				b;
-	int				a;
-}					t_color;
+	int					r;
+	int					g;
+	int					b;
+	int					a;
+}						t_color;
 
-// For 3D vectors (used for positions and directions)
 typedef struct s_vec3
 {
-	double			x;
-	double			y;
-	double			z;
-}					t_vec3;
+	double				x;
+	double				y;
+	double				z;
+}						t_vec3;
 
 typedef struct s_ray
 {
-	t_vec3			origin;
-	t_vec3			direction;
-}					t_ray;
+	t_vec3				origin;
+	t_vec3				direction;
+}						t_ray;
 
 typedef enum e_object_type
 {
@@ -44,152 +42,146 @@ typedef enum e_object_type
 	PLANE,
 	CYLINDER,
 	CONE
-}					t_object_type;
+}						t_object_type;
 
-typedef struct s_object t_object;
+typedef struct s_object	t_object;
 
 typedef struct s_hit
 {
-	double t;
-	t_vec3 point;
-	t_vec3 normal;
-	t_color color;
-	t_object	*object;
-					// Add more fields if needed (e.g., for lighting)
-}					t_hit;
+	double				t;
+	t_vec3				point;
+	t_vec3				normal;
+	t_color				color;
+	t_object			*object;
+}						t_hit;
 
 typedef struct s_object
 {
-	t_object_type	type;
-	void			*data;
-	char			*material_link;
-	t_color			color;
-	double			(*hit)(struct s_object *, const t_ray ray, t_hit *hit_info);
-}					t_object;
+	t_object_type		type;
+	void				*data;
+	char				*material_link;
+	t_color				color;
+	double				(*hit)(struct s_object *, const t_ray ray,
+			t_hit *hit_info);
+}						t_object;
 
-// Ambient lighting
 typedef struct s_ambient
 {
-	double			ratio;
-	t_color			color;
-	int is_set;
-}					t_ambient;
+	double				ratio;
+	t_color				color;
+	int					is_set;
+}						t_ambient;
 
-// Camera
 typedef struct s_camera
 {
-	t_vec3			position;
-	t_vec3			orientation;
-	int				fov;
-	int is_set;
-}					t_camera;
+	t_vec3				position;
+	t_vec3				orientation;
+	int					fov;
+	int					is_set;
+}						t_camera;
 
-// Light source
 typedef struct s_light
 {
-	t_vec3			position;
-	double			brightness;
-	t_color			color;
-}					t_light;
+	t_vec3				position;
+	double				brightness;
+	t_color				color;
+}						t_light;
 
-// Sphere
 typedef struct s_sphere
 {
-	t_vec3			center;
-	double			diameter;
-	t_color			color;
-	xpm_t			*texture;
-	xpm_t			*bump_map;
-	double			ks;
-	double			shine;
-	double			reflection;
-	bool			checker;
-	t_color			checker_black;
-	t_color			checker_white;
-	char			*material_link;
+	t_vec3				center;
+	double				diameter;
+	t_color				color;
+	xpm_t				*texture;
+	xpm_t				*bump_map;
+	double				ks;
+	double				shine;
+	double				reflection;
+	bool				checker;
+	t_color				checker_black;
+	t_color				checker_white;
+	char				*material_link;
 
-}					t_sphere;
+}						t_sphere;
 
 typedef struct s_plane
 {
-	t_vec3			position;
-	t_vec3			orientation;
-	t_color			color;
-	xpm_t			*texture;
-	xpm_t			*bump_map;
-	double			ks;
-	double			shine;
-	double			reflection;
-	bool			checker;
-	t_color			checker_black;
-	t_color			checker_white;
-	char			*material_link;
-	double			limit_width;
-	double			limit_height;
-}					t_plane;
+	t_vec3				position;
+	t_vec3				orientation;
+	t_color				color;
+	xpm_t				*texture;
+	xpm_t				*bump_map;
+	double				ks;
+	double				shine;
+	double				reflection;
+	bool				checker;
+	t_color				checker_black;
+	t_color				checker_white;
+	char				*material_link;
+	double				limit_width;
+	double				limit_height;
+}						t_plane;
 
 typedef struct s_cylinder
 {
-	t_vec3			position;
-	t_vec3			orientation;
-	double			diameter;
-	double			height;
-	t_color			color;
-	xpm_t			*texture;
-	xpm_t			*bump_map;
-	double			ks;
-	double			shine;
-	double			reflection;
-	bool			checker;
-	t_color			checker_black;
-	t_color			checker_white;
-	char			*material_link;
-}					t_cylinder;
+	t_vec3				position;
+	t_vec3				orientation;
+	double				diameter;
+	double				height;
+	t_color				color;
+	xpm_t				*texture;
+	xpm_t				*bump_map;
+	double				ks;
+	double				shine;
+	double				reflection;
+	bool				checker;
+	t_color				checker_black;
+	t_color				checker_white;
+	char				*material_link;
+}						t_cylinder;
 
 typedef struct s_cone
 {
-	t_vec3 apex;         // tip of the cone
-	t_vec3 direction;    // normalized axis from apex downward
-	double angle;        // angle in radians between axis and surface
-	double height;       // height of the cone (for truncated cone)
-	t_color color;
-	double ks;
-	double shine;
-	double reflection;
-	bool			checker;
-	t_color			checker_black;
-	t_color			checker_white;
-	char			*material_link;
-} t_cone;
+	t_vec3				apex;
+	t_vec3				direction;
+	double				angle;
+	double				height;
+	t_color				color;
+	double				ks;
+	double				shine;
+	double				reflection;
+	bool				checker;
+	t_color				checker_black;
+	t_color				checker_white;
+	char				*material_link;
+}						t_cone;
 
-
-// Main scene struct
 typedef struct s_scene
 {
-	t_ambient		ambient;
-	t_camera		camera;
-	t_light *lights; // Array of lights
-	int				light_count;
-	t_sphere *spheres; // Array of spheres
-	int				sphere_count;
-	t_plane *planes; // Array of planes
-	int				plane_count;
-	t_cylinder *cylinders; // Array of cylinders
-	int				cylinder_count;
-	t_cone *cones; // Array of cones
-	int				cone_count;
-	t_object *objects; // Array of objects (spheres, planes, etc.)
-	int				object_count;
-}					t_scene;
+	t_ambient			ambient;
+	t_camera			camera;
+	t_light				*lights;
+	int					light_count;
+	t_sphere			*spheres;
+	int					sphere_count;
+	t_plane				*planes;
+	int					plane_count;
+	t_cylinder			*cylinders;
+	int					cylinder_count;
+	t_cone				*cones;
+	int					cone_count;
+	t_object			*objects;
+	int					object_count;
+}						t_scene;
 
 typedef struct s_miniRT
 {
-	mlx_t			*mlx;
-	mlx_image_t		*img;
-	t_scene			scene;
-	bool			low_res_mode;
-	int				res_scale;
-	int				samples;
-}					t_miniRT;
+	mlx_t				*mlx;
+	mlx_image_t			*img;
+	t_scene				scene;
+	bool				low_res_mode;
+	int					res_scale;
+	int					samples;
+}						t_miniRT;
 
-# endif
+#endif
