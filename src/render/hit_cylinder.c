521e5c7b1562592bdb90d2fc5367a38d3728f4b5
lkubler <leokblr@gmail.com>
2025-05-26 10:10:42 +0200

    norm improvements for hit cylinder

--- src/render/hit_cylinder.c
+++ src/render/hit_cylinder.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hit_cylinder.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: leokubler <leokubler@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 12:50:00 by nlewicki          #+#    #+#             */
-/*   Updated: 2025/05/13 11:28:44 by nlewicki         ###   ########.fr       */
+/*   Updated: 2025/05/26 10:10:21 by leokubler        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,106 +45,73 @@ double	solve_quadratic(double a, double b, double c)
 	return (-1.0);
 }
 
-static double	hit_cylinder_caps(t_object *obj, t_cylinder *cyl, const t_ray ray, t_hit *hit_info, t_vec3 cap_center)
+// Check and update cylinder caps
+static void	check_cylinder_caps(t_cylinder_context *ctx)
 {
-	t_vec3	normal;
-	t_vec3	p;
-	double	denom;
-	double	t;
+	t_vec3	bottom_cap;
+	t_vec3	top_cap;
+	double	t_cap;
 
-	normal = cyl->orientation;
-	denom = vec_dot(ray.direction, normal);
-	if (fabs(denom) < 1e-6)
-		return (-1.0);
-	t = vec_dot(vec_sub(cap_center, ray.origin), normal) / denom;
-	if (t < 0)
-		return (-1.0);
-	p = vec_add(ray.origin, vec_mul(ray.direction, t));
-	if (vec_length(vec_sub(p, cap_center)) > (cyl->diameter / 2) + 1e-6)
-		return (-1.0);
-	if (hit_info)
+	bottom_cap = ctx->cyl->position;
+	top_cap = vec_add(ctx->cyl->position, 
+			vec_mul(ctx->cyl->orientation, ctx->cyl->height));
+	t_cap = hit_cylinder_caps(ctx->obj, ctx->cyl, ctx->ray, NULL, bottom_cap);
+	if (t_cap > 0 && t_cap < ctx->closest_t)
 	{
-		hit_info->t = t;
-		hit_info->point = p;
-		if (denom > 0)
-			hit_info->normal = normal;
-		else
-			hit_info->normal = vec_mul(normal, -1);
-		hit_info->object = obj;
+		ctx->closest_t = t_cap;
+		if (ctx->hit_info)
+			hit_cylinder_caps(ctx->obj, ctx->cyl, ctx->ray, 
+				ctx->hit_info, bottom_cap);
 	}
-	return (t);
+	t_cap = hit_cylinder_caps(ctx->obj, ctx->cyl, ctx->ray, NULL, top_cap);
+	if (t_cap > 0 && t_cap < ctx->closest_t)
+	{
+		ctx->closest_t = t_cap;
+		if (ctx->hit_info)
+			hit_cylinder_caps(ctx->obj, ctx->cyl, ctx->ray, 
+				ctx->hit_info, top_cap);
+	}
 }
 
+// Calculate cylinder body intersection
+static double	calculate_body_intersection(t_cylinder_context *ctx)
+{
+	t_cylinder_math	math;
+	double			a;
+	double			b;
+	double			c;
+
+	math.oc = vec_sub(ctx->ray.origin, ctx->cyl->position);
+	math.axis = ctx->cyl->orientation;
+	math.dot_d_axis = vec_dot(ctx->ray.direction, math.axis);
+	math.dot_oc_axis = vec_dot(math.oc, math.axis);
+	a = vec_dot(ctx->ray.direction, ctx->ray.direction) 
+		- math.dot_d_axis * math.dot_d_axis;
+	b = 2 * (vec_dot(ctx->ray.direction, math.oc) 
+		- math.dot_d_axis * math.dot_oc_axis);
+	c = vec_dot(math.oc, math.oc) - math.dot_oc_axis * math.dot_oc_axis 
+		- (ctx->cyl->diameter * ctx->cyl->diameter) / 4;
+	return (solve_quadratic(a, b, c));
+}
+
+
+
+// Main cylinder intersection function
 double	hit_cylinder(t_object *obj, const t_ray ray, t_hit *hit_info)
 {
-	t_cylinder	*cyl;
-	t_vec3		oc;
-	t_vec3		d;
-	t_vec3		axis;
-	t_vec3		bottom_cap;
-	t_vec3		top_cap;
-	t_vec3		p;
-	double		t;
-	double		t_cap;
-	double		dot_d_axis;
-	double		dot_oc_axis;
-	double		a;
-	double		b;
-	double		c;
-	double		t_body;
-	double		height;
+	t_cylinder_context	ctx;
 
-	cyl = (t_cylinder *)obj->data;
-	if (!cyl)
+	ctx.cyl = (t_cylinder *)obj->data;
+	if (!ctx.cyl)
 		return (-1.0);
-	cyl->orientation = vec_normalize(cyl->orientation);
-	bottom_cap = cyl->position;
-	top_cap = vec_add(cyl->position, vec_mul(cyl->orientation, cyl->height));
-	t = INFINITY;
-	t_cap = hit_cylinder_caps(obj, cyl, ray, NULL, bottom_cap);
-	if (t_cap > 0 && t_cap < t)
-	{
-		t = t_cap;
-		if (hit_info)
-			hit_cylinder_caps(obj, cyl, ray, hit_info, bottom_cap);
-	}
-	t_cap = hit_cylinder_caps(obj, cyl, ray, NULL, top_cap);
-	if (t_cap > 0 && t_cap < t)
-	{
-		t = t_cap;
-		if (hit_info)
-			hit_cylinder_caps(obj, cyl, ray, hit_info, top_cap);
-	}
-	oc = vec_sub(ray.origin, cyl->position);
-	d = ray.direction;
-	axis = cyl->orientation;
-	dot_d_axis = vec_dot(d, axis);
-	dot_oc_axis = vec_dot(oc, axis);
-	a = vec_dot(d, d) - dot_d_axis * dot_d_axis;
-	b = 2 * (vec_dot(d, oc) - dot_d_axis * dot_oc_axis);
-	c = vec_dot(oc, oc) - dot_oc_axis * dot_oc_axis - (cyl->diameter * cyl->diameter) / 4;
-	t_body = solve_quadratic(a, b, c);
-	if (t_body > 0 && t_body < t)
-	{
-		p = vec_add(ray.origin, vec_mul(ray.direction, t_body));
-		height = vec_dot(vec_sub(p, cyl->position), cyl->orientation);
-		if (height >= -1e-6 && height <= cyl->height + 1e-6)
-		{
-			t = t_body;
-			if (hit_info)
-			{
-				hit_info->t = t;
-				hit_info->point = p;
-				hit_info->normal = vec_normalize(vec_sub(p, vec_add(cyl->position, vec_mul(cyl->orientation, height))));
-				if (cyl->checker)
-					hit_info->color = checkerboard_cylinder(cyl, hit_info->point);
-				else
-					hit_info->color = obj->color;
-				hit_info->object = obj;
-			}
-		}
-	}
-	if (t == INFINITY)
+	ctx.obj = obj;
+	ctx.ray = ray;
+	ctx.hit_info = hit_info;
+	ctx.closest_t = INFINITY;
+	ctx.cyl->orientation = vec_normalize(ctx.cyl->orientation);
+	check_cylinder_caps(&ctx);
+	check_cylinder_body(&ctx);
+	if (ctx.closest_t == INFINITY)
 		return (-1.0);
-	return (t);
+	return (ctx.closest_t);
 }
